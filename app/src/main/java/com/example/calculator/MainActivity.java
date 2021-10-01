665995849bda0a973de0bd0230e38354f506eb18
KumarSashank <kumarsashank_ganta@srmap.edu.in>
2021-10-01 18:32:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button next;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        next=findViewById(R.id.next);
+
+        next.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                menu();
+            }
+        });
+    }
+    public void menu() {
+        Intent intent = new Intent(this, Menu_Page.class);
+        startActivity(intent);
+    }
+}
